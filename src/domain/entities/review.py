ce0145884fa4734f5e809282c6e37753752ba394
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2026-04-06 15:06:53 -0600

    feat: from raw values to objects

--- src/domain/entities/review.py
+++ src/domain/entities/review.py
@@ -1,13 +1,14 @@
 from datetime import datetime, timezone
-
+from src.domain.value_objects.review_status import ReviewStatus
+from src.domain.value_objects.rating import Rating
 APPROVAL_THRESHOLD = 70;
 
 class Review:
-    def __init__(self, pr_id: str, review_id: str | None = None):
+    def __init__(self, pr_id: str, review_id: str | None = None, rating : Rating | None = None):
         self.id = review_id or ""
         self.pr_id = pr_id
-        self.status = "pending"
-        self.rating = None
+        self.status = ReviewStatus.PENDING
+        self.rating = rating
         self.summary = ""
         self.recommendations = []
         self.completed_at = None
@@ -17,24 +18,24 @@ class Review:
         """Validate rating"""
         if self.rating is None:
             return False
-        return self.rating > APPROVAL_THRESHOLD
+        return self.rating.is_passing()
     
     def start_processing(self) -> None:
         """Start the PR"""
-        self.status = 'in_progress'
+        self.status = ReviewStatus.IN_PROGRESS
     
-    def complete(self,rating: int, summary: str, recommendations: str ) -> None:
+    def complete(self,rating: Rating, summary: str, recommendations: str ) -> None:
         """Complete the PR"""
         self.rating = rating
         self.summary = summary
         self.recommendations = recommendations
-        self.status = 'completed'
+        self.status = ReviewStatus.COMPLETED
         self.completed_at = datetime.now(timezone.utc)
         
     def fail(self, summary: str) -> None:
         """Fail PR"""
         self.summary = summary
-        self.status = 'failed'
+        self.status = ReviewStatus.FAILED
         self.completed_at = datetime.now(timezone.utc)
         
     def __repr__(self) -> str:
