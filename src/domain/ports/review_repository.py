ed27fc2dcd15ed801878596bcfb263b17b8713ba
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2026-04-06 15:43:57 -0600

    feat: clase2 test pasando

--- src/domain/ports/review_repository.py
+++ src/domain/ports/review_repository.py
@@ -1,2 +1,24 @@
-class IReviewRepository:
+from abc import ABC, abstractmethod
+
+class IReviewRepository(ABC):
     """ repo """
+
+    @abstractmethod
+    def save(self):
+        pass
+    
+    @abstractmethod
+    def find_by_pr_id(self):
+        pass
+    
+    @abstractmethod
+    def find_by_id(self):
+        pass
+
+    @abstractmethod
+    def find_all(self):
+        pass
+    
+    @abstractmethod
+    def delete(self):
+        pass
