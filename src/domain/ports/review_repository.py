f6a494b18f5b4f500e3e5faec06b5a7c53eb5ef9
FrancoTastaca <franc0tastac@gmail.com>
2026-04-07 16:05:54 -0300

    clase-2 resuelvo tests - Franco Tastaca

--- src/domain/ports/review_repository.py
+++ src/domain/ports/review_repository.py
@@ -1,2 +1,23 @@
-class IReviewRepository:
-    """ repo """
+from abc import ABC, abstractmethod
+from src.domain.entities.review import Review
+
+class IReviewRepository(ABC):
+    @abstractmethod 
+    def save( self, review: Review) -> Review:
+        return True
+   
+    @abstractmethod 
+    def find_by_pr_id(self, pr_id: str) -> (Review | None): 
+        return True
+    
+    @abstractmethod 
+    def find_by_id(self, review_id: str) -> (Review | None): 
+        return True
+    
+    @abstractmethod
+    def find_all(self) -> list[Review]: 
+        return True
+    
+    @abstractmethod
+    def delete(self, review_id: str) -> bool: 
+        return True
