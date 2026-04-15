9d10610376acf3f25505c857760600832cfd68ea
Gabriel Vargas <gabilol.soad@gmail.com>
2026-04-15 12:10:09 -0300

    clase-2: resuelvo tests - Gabriel Vargas

--- src/domain/ports/review_repository.py
+++ src/domain/ports/review_repository.py
@@ -1,2 +1,19 @@
-class IReviewRepository:
-    """ repo """
+from abc import ABC, abstractmethod
+from src.domain.entities.review import Review
+
+class IReviewRepository(ABC):
+    @abstractmethod
+    def save(self, review: Review) -> Review:
+        """ save """
+    @abstractmethod
+    def find_by_pr_id(self, pr_id: str) -> Review | None:
+        """ find_by_pr_id """
+    @abstractmethod
+    def find_by_id(self, review_id: str) -> Review | None:
+        """ find_by_id """
+    @abstractmethod
+    def find_all(self) -> list[Review]:
+        """ find_all """
+    @abstractmethod
+    def delete(self, review_id: str) -> bool:
+        """ delete """
