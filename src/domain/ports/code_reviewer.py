1a195963c1a779a22e48857e652889dd87cf352a
lucianux <lucianux@gmail.com>
2026-04-19 00:06:41 -0300

    test: TestCodeContent performed

--- src/domain/ports/code_reviewer.py
+++ src/domain/ports/code_reviewer.py
@@ -10,7 +10,11 @@ class ICodeReviewer(ABC):
     None
 
 class CodeContent(ABC):
-  None
+  def __init__(self, files: dict[str,str], folder_structure:list[str], pr_title: str, pr_description: str):
+    self.files = files
+    self.folder_structure = folder_structure
+    self.pr_title = pr_title
+    self.pr_description = pr_description
 
 class ReviewResult():
   def __init__(self, rating: int, summary: str, recommendations: list[str]):
